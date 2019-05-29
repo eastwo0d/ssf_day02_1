bc3866213af44255c154a8927e19b54a22f37f9d
eastwo0d <roystonchua85@gmail.com>
2019-05-29 09:00:11 +0800

    randNum

--- main.js
+++ main.js
@@ -10,7 +10,20 @@ const app = express();
 //Routing
 app.use(express.static(__dirname + '/public'));
 
+app.get('/number', (req,res)=>{
+    res.status(200);
+    res.type('text/html');
+    res.send(`<img src="${randNum()}">`);
+})
+
 app.listen(PORT, ()=> {
     console.info(`Application started on port ${PORT} at ${new Date()}`);
 })
 
+function randNum() {
+    var randImg = Math.floor(Math.random()*31);
+    var imgName = "/images/number" + randImg + ".jpg";
+    console.log(imgName);
+    return imgName;
+    
+}
